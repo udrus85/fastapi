4d035bee8403b6655086859366ca66c9b271e5ba
Rus <1@example.com>
2026-04-19 03:21:25 +0500

    2

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,4 +1,4 @@
-#routers/tasks.py
+# routers/tasks.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -11,14 +11,18 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 
 @router.post("/", response_model=Task)
-def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+def create_task(
+        task: TaskCreate,
+        db: Session = Depends(get_db),
+        current_user=Depends(get_current_user)  # ← ДОБАВЬТЕ
+):
     return crud_tasks.create_task(db, task)
 
 
 @router.get("/")
 def read_tasks(
-    db: Session = Depends(get_db),
-    current_user = Depends(get_current_user)
+        db: Session = Depends(get_db),
+        current_user=Depends(get_current_user)
 ):
     return crud_tasks.get_tasks(db)
 
--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,10 @@
-#routers/users.py
+# routers/users.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 import crud.users as crud_users
 from database import get_db
+from dependencies.auth import get_current_user
 import schemas
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -15,5 +16,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=list[schemas.User])
-def read_users(db: Session = Depends(get_db)):
+def read_users(
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)  # ← ДОБАВЬТЕ
+):
     return crud_users.read_users(db)
